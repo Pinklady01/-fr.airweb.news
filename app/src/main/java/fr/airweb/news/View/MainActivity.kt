e79d61a0d214decf9886a7dec448ff5a4da9413a
marion <katyanne_mm@live.fr>
2019-11-20 00:10:52 +0100

    Retrofit Service

--- app/src/main/java/fr/airweb/news/View/MainActivity.kt
+++ app/src/main/java/fr/airweb/news/View/MainActivity.kt
@@ -1,13 +1,23 @@
-package fr.airweb.news.View
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import fr.airweb.news.Model.RetrofitClientInstance
 import fr.airweb.news.R
+import fr.airweb.news.ViewModel.GetNewService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        RetrofitClientInstance.retrofitInstance?.create(GetNewService::class.java)
     }
+
+
 }
