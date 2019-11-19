a1b35bdc8020f6d16355e012091de7e44bcf8699
marion <katyanne_mm@live.fr>
2019-11-19 11:27:46 +0100

    first commit

---
+++ app/src/main/java/fr/airweb/news/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package fr.airweb.news.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.airweb.news.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
